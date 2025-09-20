7b153c57ac37b61c456423df351f575fec861de0
DJUROM777 <djurom777@ya.ru>
2025-09-20 14:03:27 +0300

    Initial Commit

---
+++ TH76/TH76App.swift
@@ -0,0 +1,17 @@
+//
+//  TH76App.swift
+//  TH76
+//
+//  Created by IGOR on 20/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct TH76App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TH76Tests/TH76Tests.swift
@@ -0,0 +1,16 @@
+//
+//  TH76Tests.swift
+//  TH76Tests
+//
+//  Created by IGOR on 20/09/2025.
+//
+
+import Testing
+
+struct TH76Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
